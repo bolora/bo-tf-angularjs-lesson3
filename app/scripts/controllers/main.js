1ceb8cbe32daffd7a78775a298b41ac21c819d3b
Bo Lora <bo_lora@dell.com>
2014-05-23 09:49:07 -0500

    finished waitstaf calculator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('boTfAngularjsLesson3App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
